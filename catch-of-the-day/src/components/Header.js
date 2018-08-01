5ae9dcf3a0129fce7ec3708e5c356d1c429b564f
Daniel Piston <dpiston@kbra.com>
2018-08-01 15:35:15 -0400

    update

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Header = props => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">of</span>
+        <span className="the">the</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
+
+// ^^ Since the component is just being passed in data via the props, we can turn it into a stateless functional component
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header className="top">
+//         <h1>
+//           Catch
+//           <span className="ofThe">
+//             <span className="of">of</span>
+//             <span className="the">the</span>
+//           </span>
+//           Day
+//         </h1>
+//         <h3 className="tagline">
+//           <span>{this.props.tagline}</span>
+//         </h3>
+//       </header>
+//     );
+//   }
+// }
+
+export default Header;
