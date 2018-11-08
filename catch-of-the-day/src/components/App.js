fdacb328158c97a0350db240df3d5e97ae4f731d
Dan Piston <danpiston@gmail.com>
2018-11-08 06:28:47 -0500

    catching up project
    Up to lesson 16

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,16 +2,39 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+  addFish = fish => {
+    // take a copy of current state
+    const fishes = { ...this.state.fishes };
+    // add new fish to state
+    fishes[`fish${Date.now()}`] = fish;
+    // set the new fishes object to state
+    this.setState({ fishes });
+    console.log(this.state);
+  };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]}/>
+            ))}
+          </ul>
         </div>
-        <Inventory />
         <Order />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
